2577b7d46e976a5eb13f1a1ebc68cc371142b5d8
thanhnm <thanhnm@vmsco.vn>
2025-08-24 10:36:17 +0700

    chore: replace folder

--- src/stores/ReduxProvider.tsx
+++ src/stores/ReduxProvider.tsx
@@ -6,7 +6,7 @@ import type { ReactNode } from 'react';
 // Third-party Imports
 import { Provider } from 'react-redux';
 
-import { store } from '@/redux-store';
+import { store } from '@/stores';
 
 const ReduxProvider = ({ children }: { children: ReactNode }) => {
     return <Provider store={store}>{children}</Provider>;
