b71d883a27c7468623653aaf53f4a38aa9eb8c2c
thanhnm <thanhnm@vmsco.vn>
2025-08-25 20:04:41 +0700

    refactor: change & remove file upload form template

--- src/components/templates/LandingTemplate.tsx
+++ src/components/templates/LandingTemplate.tsx
@@ -1,12 +1,25 @@
 'use client';
 
+import { ArrowBigDown, ArrowBigRight } from 'lucide-react';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 import Logo from '../atoms/Logo';
 import { Button } from '../ui/button';
 
+const CanvasFrame = dynamic(
+    () => import('@/components/organisms/CanvasFrame'),
+    { ssr: false }
+);
+
 export default function LandingTemplate() {
     const router = useRouter();
+
+    const [isAction, setIsAction] = useState(false);
+    const [photo1, setPhoto1] = useState<string | null>(null);
+    const [photo2, setPhoto2] = useState<string | null>(null);
+
     return (
         <section className='relative min-h-screen w-full flex flex-col items-center justify-start overflow-hidden'>
             <Image
@@ -35,25 +48,61 @@ export default function LandingTemplate() {
                 </p>
 
                 <div className='mt-10 shadow-xl border border-amber-200'>
-                    <Image
-                        src='/images/landing-page-frame-preview.png'
-                        alt='Demo'
-                        width={800}
-                        height={800}
-                        className='object-cover'
-                        priority
-                    />
+                    {isAction ? (
+                        <CanvasFrame
+                            width={800}
+                            height={800}
+                            frameSrc='/images/frame.png'
+                            photos={[photo1, photo2]}
+                            setPhoto={(i, url) => {
+                                if (i === 0) setPhoto1(url);
+                                if (i === 1) setPhoto2(url);
+                            }}
+                            slots={[
+                                { x: 110, y: 230, width: 260, height: 340 },
+                                { x: 450, y: 180, width: 260, height: 340 },
+                            ]}
+                        />
+                    ) : (
+                        <Image
+                            src='/images/landing-page-frame-preview.png'
+                            alt='Demo'
+                            width={800}
+                            height={800}
+                            className='object-cover'
+                            priority
+                        />
+                    )}
                 </div>
 
                 <div className='flex gap-6 mt-16'>
-                    <Button
-                        variant='cta'
-                        size='xl'
-                        className='w-full text-[40px] leading-none tracking-tight font-extrabold'
-                        onClick={() => router.push('/create')}
-                    >
-                        BẮT ĐẦU TẠO
-                    </Button>
+                    {isAction ? (
+                        <>
+                            <Button
+                                variant='cta'
+                                size='xl'
+                                className='text-[40px] font-extrabold'
+                            >
+                                Tải xuống <ArrowBigDown className='size-10' />
+                            </Button>
+                            <Button
+                                variant='cta'
+                                size='xl'
+                                className='text-[40px] font-extrabold'
+                            >
+                                Chia sẻ <ArrowBigRight className='size-10' />
+                            </Button>
+                        </>
+                    ) : (
+                        <Button
+                            variant='cta'
+                            size='xl'
+                            className='w-full text-[40px] leading-none tracking-tight font-extrabold'
+                            onClick={() => setIsAction(!isAction)}
+                        >
+                            BẮT ĐẦU TẠO
+                        </Button>
+                    )}
                 </div>
 
                 <div className='flex gap-6 mt-41 w-[100%]'>
